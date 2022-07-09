4cde80a135ee126893b7dafa1866deb06223e3f8
Suresh Kumar <gfamcode8955@gmail.com>
2022-07-09 15:47:55 +0530

    initial commit

---
+++ src/main/java/com/suresh/downloadfroms3/DownloadFromS3Application.java
@@ -0,0 +1,13 @@
+package com.suresh.downloadfroms3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadFromS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadFromS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/suresh/downloadfroms3/DownloadFromS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.downloadfroms3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadFromS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
